a3d878546d40d5d3a8ffb43241accfaeff142274
Pufferfish <tanzeeltariq39@gmail.com>
2021-07-01 02:10:39 +0500

    auth middleware

---
+++ app/Http/Middleware/CheckAuth.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+class CheckAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check() || $request -> is ('login') || $request -> is ('register') ) {
+            return $next($request);
+        } else {
+            return redirect('/login');
+        }
+    }
+}
