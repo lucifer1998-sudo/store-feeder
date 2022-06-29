e98d9b718580f4e4db052f0b80127f996db83312
Muhammad_Rehan <irehanmehar@gmail.com>
2022-06-29 16:47:35 +0500

    point7

--- app/Http/Controllers/LogsController.php
+++ app/Http/Controllers/LogsController.php
@@ -40,7 +40,7 @@ class LogsController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->all());
+//        dd($request->all());
         $attachment = '';
         if (isset($request -> file)){
             $file = $request -> file;
